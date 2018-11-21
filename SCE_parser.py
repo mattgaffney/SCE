296eb3bb2eb88bb5d46da43cc803b1d7dffe0019
Matt Gaffney <mattgaffney96@gmail.com>
2018-11-21 00:18:04 +0000

    fixed arff, commented and planned

--- SCE_parser.py
+++ SCE_parser.py
@@ -14,7 +14,7 @@ import arff
 
 
 
-pset = gp.PrimitiveSet("MAIN", 1)
+pset = gp.PrimitiveSet("MAIN", 1) # number of argument should be length of variables??  list of variables?
 pset.addPrimitive(operator.add, 2)
 pset.addPrimitive(operator.sub, 2)
 pset.addPrimitive(operator.mul, 2)
@@ -24,10 +24,40 @@ pset.addPrimitive(operator.neg, 1)
 pset.addEphemeralConstant("rand101", lambda: random.randint(-1, 1)) #do i need????
 pset.renameArguments(ARG0='x') #do i need????
 
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+
+
+toolbox = base.Toolbox()
+toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("compile", gp.compile, pset=pset)
+
+
+#
+def evaluate(individual, variables, effort):
+    # randomly select a number of entries
+
+    # compile tree and plug in variables
+    func = toolbox.compile(expr=individual)
+    #fitness =(sum(func(variables[i]) - efffort[i])**2)**0.5
+
+    return (fitness) # must be tuple
+
+
+toolbox.register("evaluate", evaluate, points=[x/10. for x in range(-10,10)])
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("mate", gp.cxOnePoint)
+toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
+toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
 
 
 def main():
-    data = arff.load(open('Data/china.arff', 'rb'))
+    data = arff.load(open('Data/china.arff'))
+    ##seperate data into variables and effort
+
+    ## run population as in example
     print()
 
 
